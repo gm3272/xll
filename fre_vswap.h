dc92a460e7be71f41795206486c0194f75c44a1e
gm3272 <147676048+gm3272@users.noreply.github.com>
2023-12-21 17:25:10 -0500

    Add files via upload

--- fre_vswap.h
+++ fre_vswap.h
@@ -20,8 +20,7 @@ namespace fre::vswap {
             ensure(n >= 2);
             ensure(std::is_sorted(x.begin(), x.end()));
         }
-
-        // value at x
+       // value at x
         double value(double x_) const {
             size_t i = index(x_);
             if (i == x.size() - 1) {
@@ -30,8 +29,7 @@ namespace fre::vswap {
             double m = (y[i + 1] - y[i]) / (x[i + 1] - x[i]);
             return y[i] + m * (x_ - x[i]);
         }
-
-        // first derivative at x
+       // first derivative at x
         double derivative(double x_) const {
             size_t i = index(x_);
             if (i == x.size() - 1) {
@@ -39,8 +37,7 @@ namespace fre::vswap {
             }
             return (y[i + 1] - y[i]) / (x[i + 1] - x[i]);
         }
-
-        // second derivative at x[1], ..., x[n-2]
+       // second derivative
         std::vector<double> delta() const {
             std::vector<double> deltas(x.size() - 1, 0.0); // Zero for linear segments
             return deltas;
@@ -51,13 +48,8 @@ namespace fre::vswap {
     double variance(double f, size_t n, const double* k, const double* p, const double* c) {
         double var = 0.0;
         for (size_t i = 0; i < n; ++i) {
-        double v = -2 * log(k[i] / f) + 2 * (k[i] - f) / f;
-        
-        if (k[i] < f) {           
-            var += v * p[i];
-        } else {
-            var += v * c[i];
-        }
+        double v = -2 * log(k[i] / f) + 2 * (k[i] - f) / f;   
+        var += (k[i] < f) ? v * p[i] : v * c[i]
     }
 
     return var / n; 
--- xll_vswap.cpp
+++ xll_vswap.cpp
@@ -8,11 +8,11 @@ using namespace xll;
 AddIn xai_vswap(
 	Function(XLL_DOUBLE, "xll_vswap", "XLL.VSWAP")
 	.Arguments({
-		Arg(XLL_DOUBLE, "f", "is the f(x)."),
-		Arg(XLL_DOUBLE, "n", "is the size of time."),
-		Arg(XLL_DOUBLE, "k", "is the strike price."),
-		Arg(XLL_DOUBLE, "p", "is the put value."),
-		Arg(XLL_DOUBLE, "c", "is the call value."),
+		Arg(XLL_DOUBLE, "f", "f(x)."),
+		Arg(XLL_DOUBLE, "n", "is the length of time."),
+		Arg(XLL_DOUBLE, "k", "is the arrary of strike price."),
+		Arg(XLL_DOUBLE, "p", "is the array of put values."),
+		Arg(XLL_DOUBLE, "c", "is the array of call values."),
 		})
 	.Category(CATEGORY)
 	.FunctionHelp("Return par variance")
@@ -23,5 +23,5 @@ double WINAPI xll_vswap(double f, size_t n, const double* k, const double* p, do
 #pragma XLLEXPORT
 	ensure(size(*k) == n && size(*p) == n && size(*c) == n);
 
-	return fre::vswap::variance(f, n, k, p, c);
+	return fre::vswap::variance(f, n, &k->array[0], &p->array[0], &c->array[0]);
 }
